170a00e7b7888d825469d14f9ee68e5f1b07eb5c
baylar <dev.ibrahimov@gmail.com>
2021-10-23 18:03:06 +0400

    create Login page and authentication , Login and Logout

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,9 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('number')->nullable();
             $table->string('email')->unique();
+            $table->string('role')->default(0);
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/login',function (){  return view('login'); })->name('login');
+Route::post('/login', [\App\Http\Controllers\General\AuthController::class, 'logincontrol']) ;
+
+Route::group(['middleware'=>'auth'],function () {
+    Route::post('/exit', [\App\Http\Controllers\General\AuthController::class, 'logout'])->name('logout');
+
+    Route::get('/', [\App\Http\Controllers\General\GeneralController::class, 'dashboard'])->name('dashboard');
+
 });
