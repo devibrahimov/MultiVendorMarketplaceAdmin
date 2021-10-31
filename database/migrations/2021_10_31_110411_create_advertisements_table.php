c39b5d7ec45aa3d9f36d45c282ae1cbe5fcdda58
Mustafayev Mustafa <mustafa.mustafa.99.99@mail.ru>
2021-10-31 15:18:23 +0400

    Database Struktur

---
+++ database/migrations/2021_10_31_110411_create_advertisements_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAdvertisementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('advertisements', function (Blueprint $table) {
+            $table->id();
+            $table->string('image')->nullable();
+            $table->string('company')->nullable();
+            $table->longText('url')->nullable();
+            $table->string('type')->nullable();
+            $table->string('page')->nullable();
+            $table->string('line')->nullable();
+            $table->dateTime('start_time');
+            $table->dateTime('stop_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('advertisements');
+    }
+}
---
+++ database/migrations/2021_10_31_110824_create_settings_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('title')->nullable();
+            $table->longText('meta_description')->nullable();
+            $table->longText('meta_tags')->nullable();
+            $table->string('logo_color')->nullable();
+            $table->string('logo_white')->nullable();
+            $table->string('logo_black')->nullable();
+            $table->string('favicon')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('settings');
+    }
+}
