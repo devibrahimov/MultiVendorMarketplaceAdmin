af09619c6d7c0d90f45d63dcfdbda581d369a4a0
baylar <dev.ibrahimov@gmail.com>
2022-01-01 16:33:07 +0400

    magaza register sehifesi yaradildi,satin fortu yuklendi ve header footer ayrildi view stucturesi hazirlandi

---
+++ resources/views/site/pages/general/home.blade.php
@@ -0,0 +1,27 @@
+@extends('welcome')
+
+@section('css')
+
+@endsection
+
+
+@section('content')
+<div class="container-fluid">
+        <div class="block-header">
+            <h2>Dashboard / </h2>
+        </div>
+        <!-- Input -->
+@include('partials.alerts')
+
+
+</div>
+<!-- end container-fluid -->
+@endsection
+
+@section('js-libs')
+
+@endsection
+
+@section('js-script')
+
+@endsection
