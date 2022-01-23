c3c04cd66efed4ab476fb71fe4c696a45da6d503
baylar <dev.ibrahimov@gmail.com>
2022-01-23 17:23:33 +0400

    product controller hit artirma  ve trend cagirma

--- app/Http/Controllers/Site/ProductController.php
+++ app/Http/Controllers/Site/ProductController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Site;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use App\Models\ProductStatistics;
-use App\Models\Setting;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
@@ -18,7 +17,13 @@ class ProductController extends Controller
     }
 
     public function productdetail(Request $request){
-        $product = Product::getFindProduct($request->key );
+        $product = Product::getFindProduct($request->key);
+
+
+        $statistic = ProductStatistics::firstOrCreate(['product_id' => $product->id]);
+        $statistic->hit += 1 ;
+        $statistic->save();
+
        if ($product ==null or $product == false or $product == false){
            return redirect()->route('site.products');
        }else{
@@ -26,11 +31,17 @@ class ProductController extends Controller
        }
 
     }
+
+
+
+
     public function trend(){
 
-        $products = ProductStatistics::where('hit','>',6)
+        $products = ProductStatistics::where('hit','>',0)
+            ->orderBy('hit','DESC')
             ->join('products','products.id','=','product_statistics.product_id')
             ->where('access',1)
+            ->select(['key','images', 'slug', 'sku', 'barkode', 'name', 'sale_price','price',])
             ->get();
 
         return view('site.pages.general.productsListPage', compact( 'products'));
